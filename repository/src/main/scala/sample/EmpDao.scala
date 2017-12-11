4b19aedf210f729714e0f62de91d8087b1f6a084
hosonishi <hosonishi@gmail.com>
2017-12-11 18:21:34 +0900

    remove iterator

--- repository/src/main/scala/sample/EmpDao.scala
+++ repository/src/main/scala/sample/EmpDao.scala
@@ -6,12 +6,11 @@ import domala.jdbc.Result
 @Dao
 trait EmpDao {
 
-  def selectById(id: ID[Emp]): Option[Emp] = selectByIds(Seq(id)) {
-    _.toStream.headOption
-  }
+  @Select("select * from emp where id = /* id */0")
+  def selectById(id: ID[Emp]): Option[Emp]
 
   @Select("select * from emp")
-  def selectAll: Seq[Emp] // scala.Seqが使える
+  def selectAll: Seq[Emp]
 
   @Insert
   def insert(emp: Emp): Result[Emp]
@@ -33,11 +32,5 @@ trait EmpDao {
   create sequence emp_id_seq start with 1;
   """)
   def create(): Unit
-
-  @Select(
-    "select * from emp where id in /* id */()",
-    strategy = SelectType.ITERATOR
-  )
-  def selectByIds[R](id: Iterable[ID[Emp]])(mapper: Iterator[Emp] => R): R
 
 }
