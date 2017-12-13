a69847874dc351966c6ccc9c2e6d1e1b0144fc31
hosonishi <hosonishi@gmail.com>
2017-12-13 19:59:22 +0900

    2.2

--- app/sample/SampleJsonConverter.scala
+++ app/sample/SampleJsonConverter.scala
@@ -7,14 +7,14 @@ import play.api.libs.functional.syntax._
 
 object EmpConverter {
 
-  implicit def writesID[T] = Json.writes[ID[T]]
-  implicit def readsID[T] = Json.reads[ID[T]]
+  implicit def writesID[T] = Writes[ID[T]] { case ID(value) => JsNumber(value) }
+  implicit def readsID[T] = Reads[ID[T]] { json => json.validate[Int] map (value => ID[T](value)) }
 
-  implicit def writesName = Json.writes[Name]
-  implicit def readsName = Json.reads[Name]
+  implicit def writesName[T] = Writes[Name] { case Name(value) => JsString(value) }
+  implicit def readsName[T] = Reads[Name] { json => json.validate[String] map (value => Name(value)) }
 
-  implicit def writesAge = Json.writes[Age]
-  implicit def readsAge = Json.reads[Age]
+  implicit def writesAge[T] = Writes[Age] { case Age(value) => JsNumber(value) }
+  implicit def readsAge[T] = Reads[Age] { json => json.validate[Int] map (value => Age(value)) }
 
   implicit def writesEmp = Json.writes[Emp]
   implicit def readsEmp = Json.reads[Emp]
