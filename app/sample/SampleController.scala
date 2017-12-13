cb0dc2ff86024a0cbce3ff515668875ea605ad67
hosonishi <hosonishi@gmail.com>
2017-12-13 09:53:32 +0900

    remove routes Other than findAll

--- app/sample/SampleController.scala
+++ app/sample/SampleController.scala
@@ -18,52 +18,12 @@ class SampleController @Inject()
 
   lazy val dao: EmpDao = DaoProvider.get[EmpDao](config)
 
-  def selectById(id: Int) = Action.async {
-    Future { Required {
-      dao.selectById(ID(id))
-    }}.map {
-      case Some(emp) => Ok(Json.toJson(emp))
-      case None => NotFound("not found.")
-    }
-  }
-
   def selectAll = Action.async {
     Future { Required {
       dao.selectAll
     }} map {
       case Nil => NotFound("not found.")
       case emps => Ok(Json.toJson(emps))
-    }
-  }
-
-  import scala.language.implicitConversions
-  import scala.language.reflectiveCalls
-  implicit def as(request: Request[AnyContent]) = new {
-    def asEmp = request.body.asJson.map(_.as[Emp])
-      .getOrElse(throw new RuntimeException("Request body colud not parse"))
-  }
-
-  def insert = Action.async { request =>
-    Future { Required {
-      dao.insert(request.asEmp)
-    }} map { result => 
-      Ok(Json.toJson(result))
-    }
-  }
-
-  def update = Action.async { request =>
-    Future { Required {
-      dao.update(request.asEmp)
-    }} map { result =>
-      Ok(Json.toJson(result))
-    }
-  }
-
-  def delete(id: Int) = Action.async {
-    Future { Required {
-      dao.selectById(ID(id)).map(dao.delete)
-    }} map { result =>
-      Ok(Json.toJson(result))
     }
   }
 
